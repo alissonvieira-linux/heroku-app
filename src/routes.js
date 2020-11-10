e04f63ab603a37614a450184e6e021aed08bc506
Alisson Vieira <alissonvieira.github@gmail.com>
2020-11-10 20:03:17 -0300

    Adiciona cors e rota de usuários

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,17 @@ const express = require('express');
 
 const routes = express.Router();
 
+const users = [
+  { id: 1, name: 'Alisson Vieira', age: 18 },
+  { id: 2, name: 'Developer', age: 21 }
+]
+
 routes.get('/', (request, response) => {
   return response.json({ mensagem: "Hello World" });
+});
+
+routes.get('/users', (request, response) => {
+  return response.json(users);
 });
 
 module.exports = routes;
